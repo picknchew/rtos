a7e2b6d09d105c5b7d72a60d5382db91476c48f4
Franco Chen <3619860+picknchew@users.noreply.github.com>
2023-09-21 10:07:03 -0400

    Hello World

--- kern.c
+++ kern.c
@@ -1,4 +1,10 @@
+#include "rpi.h";
 
 int kmain() {
-  
+  const int CONSOLE = 0;
+
+  uart_config_and_enable(CONSOLE, 115200, false);
+  uart_init();
+  uart_puts(CONSOLE, "Hello, world!\r\n");
+  return 0;
 }
