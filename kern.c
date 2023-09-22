fda4eda136d8baf7df3ea8e1a76ad161c1de7521
Franco Chen <3619860+picknchew@users.noreply.github.com>
2023-09-22 16:17:34 -0400

    simple exception handler

--- kern.c
+++ kern.c
@@ -1,15 +1,26 @@
 #include "rpi.h"
 
-#define VBAR_EL1
+#define SVC(code) asm volatile ("svc %0" : : "I" (code) )
 
 //https://github.com/s-matyukevich/raspberry-pi-os/blob/master/docs/lesson03/rpi-os.md
 //https://github.com/isometimes/rpi4-osdev/tree/master/part13-interrupts
 
-int kmain() {
-  const int CONSOLE = 0;
+const int CONSOLE = 1;
 
+void exception_handler() {
+  uart_puts(CONSOLE, "exception handler\r\n");
+
+  for (;;) {}
+
+  uart_puts(CONSOLE, "after loop\r\n");
+}
+
+int kmain() {
   uart_config_and_enable(CONSOLE, 115200, false);
   uart_init();
-  uart_puts(CONSOLE, "Hello, world!\r\n");
+  uart_puts(CONSOLE, "kmain called\r\n");
+
+  SVC(0);
+
   return 0;
 }
