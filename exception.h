3c4f030712e5ce64760bcf58b0b1ad2c5920341c
Chenxi Liu <c265liu@uwaterloo.ca>
2023-09-25 18:51:48 -0400

    Update exception.h

--- exception.h
+++ exception.h
@@ -1,3 +1,9 @@
 #pragma once
 
 void handle_exception(int exception);
+int syscall_create(struct TaskDescriptor *task, int priority,void (*code)());
+int syscall_my_tid(struct TaskDescriptor *task);
+int syscall_my_parent_tid(struct TaskDescriptor *task);
+void syscall_yield();
+void syscall_exit(struct TaskDescriptor *current);
+
