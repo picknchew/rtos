0f08990115ffb484f5b60b7f3113c7c5f7693fe7
Chenxi Liu <c265liu@uwaterloo.ca>
2023-11-01 17:40:15 -0400

    update

--- user/terminal/velocity_task.c
+++ user/terminal/velocity_task.c
@@ -12,15 +12,12 @@ static char *switch_init[8] =
     {"sw 14 C", "sw 13 S", "sw 10 S", "sw 9 C", "sw 8 C", "sw 17 S", "sw 16 S", "sw 15 C"};
 static int switchc_len[8] = {7, 7, 7, 6, 6, 7, 7, 7};
 
-const char *TRAINS[] = {"1", "2", "24", "47", "54", "58", "77", "78"};
-static int trains_len[] = {1, 1, 2, 2, 2, 2, 2, 2};
-const char *TRAINSET_SPEED[] =
-    {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14"};
+// const char *TRAINS[] = {"1", "2", "24", "47", "54", "58", "77", "78"};
+// static int trains_len[] = {1, 1, 2, 2, 2, 2, 2, 2};
+// const char *TRAINSET_SPEED[] =
+//     {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14"};
 
 void velocity_measurement_task() {
-  FILE *fp;
-  fp = fopen("data.log", "w");
-  //   fprintf(fp, "hello");
   RegisterAs("vmeasurement");
   struct VelocityMeasurementInfo info;
   //   int terminal = MyParentTid();
@@ -44,12 +41,13 @@ void velocity_measurement_task() {
   int time_tid = WhoIs("clock_server");
   for (int k = 0; k < 8; k++) {
     for (int m = 0; m < 15; m++) {
-      strcat(speed_command, "tr ", 3);
-      strcat(speed_command, TRAINS[k], trains_len[k]);
-      strcat(speed_command, " ", 1);
-      strcat(speed_command, TRAINSET_SPEED[m], m / 10 + 1);
-      msglen = 4 + trains_len[k] + m / 10 + 1;
-      memcpy(terminal->command_buffer, speed_command, msglen);
+    //   strcat(speed_command, "tr ", 3);
+    //   strcat(speed_command, TRAINS[k], trains_len[k]);
+    //   strcat(speed_command, " ", 1);
+    //   strcat(speed_command, TRAINSET_SPEED[m], m / 10 + 1);
+    //   msglen = 4 + trains_len[k] + m / 10 + 1;
+    //   memcpy(terminal->command_buffer, speed_command, msglen);
+      TrainSetSpeed(train_tid,TRAINSET_TRAINS[k],m);
       for (int i = 0; i < 100; i++) {
         Receive(&tid, msg, 1);
         Reply(tid, NULL, 0);
@@ -60,10 +58,9 @@ void velocity_measurement_task() {
         if (i >= 2) {
           t += t2 - t1;
         }
-        fprintf(fp, "train: %d speed:%d time:%d\r\n", k, t, t2 - t1);
+        printf("train: %d speed:%d time:%d\r\n",k,t,t2-t1);
       }
     }
   }
-
   Exit();
 }
