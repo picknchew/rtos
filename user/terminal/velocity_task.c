fb56e8715301a48fb03e2efd73532ee82dc0e83b
Chenxi Liu <c265liu@uwaterloo.ca>
2023-11-01 15:16:48 -0400

    test

--- user/terminal/velocity_task.c
+++ user/terminal/velocity_task.c
@@ -4,18 +4,18 @@
 
 #include "terminal_task.h"
 #include "util.h"
+#include "../server/name_server.h"
+#include "../../syscall.h"
+#include "../server/clock_server.h"
 
 static char *switch_init[8] =
     {"sw 14 C", "sw 13 S", "sw 10 S", "sw 9 C", "sw 8 C", "sw 17 S", "sw 16 S", "sw 15 C"};
 static int switchc_len[8] = {7, 7, 7, 6, 6, 7, 7, 7};
 
-const char *TRAINSET_TRAINS[] = {"1", "2", "24", "47", "54", "58", "77", "78"};
+const char *TRAINS[] = {"1", "2", "24", "47", "54", "58", "77", "78"};
 static int trains_len[] = {1, 1, 2, 2, 2, 2, 2, 2};
 const char *TRAINSET_SPEED[] =
     {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14"};
-static char *switch_init[8] =
-    {"sw 14 C", "sw 13 S", "sw 10 S", "sw 9 C", "sw 8 C", "sw 17 S", "sw 16 S", "sw 15 C"};
-static int switchc_len[8] = {7, 7, 7, 6, 6, 7, 7, 7};
 
 void velocity_measurement_task() {
   FILE *fp;
@@ -35,17 +35,17 @@ void velocity_measurement_task() {
     terminal->command_buffer[switchc_len[i]] = '\0';
     terminal_execute_command(terminal, train_tid, terminal->command_buffer);
     terminal_clear_command_buffer(terminal);
-    terminal_update_command_buffer(terminal);
   }
 
   char msg[2];
   int msglen = 0;
   char speed_command[10];
   int t = 0;
+  int time_tid = WhoIs("clock_server");
   for (int k = 0; k < 8; k++) {
     for (int m = 0; m < 15; m++) {
       strcat(speed_command, "tr ", 3);
-      strcat(speed_command, TRAINSET_TRAINS[k], trains_len[k]);
+      strcat(speed_command, TRAINS[k], trains_len[k]);
       strcat(speed_command, " ", 1);
       strcat(speed_command, TRAINSET_SPEED[m], m / 10 + 1);
       msglen = 4 + trains_len[k] + m / 10 + 1;
@@ -53,10 +53,10 @@ void velocity_measurement_task() {
       for (int i = 0; i < 100; i++) {
         Receive(&tid, msg, 1);
         Reply(tid, NULL, 0);
-        int t1 = Time();
+        int t1 = Time(time_tid);
         Receive(&tid, msg, 1);
         Reply(tid, NULL, 0);
-        int t2 = Time();
+        int t2 = Time(time_tid);
         if (i >= 2) {
           t += t2 - t1;
         }
